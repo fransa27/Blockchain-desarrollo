5f37e928d899c0de533ed49f711f88df336ed183
fvsoto <102995723+fransa27@users.noreply.github.com>
2024-10-21 23:07:47 -0300

    actualizacion de Marketplace.test.js y arregloo del fomrulario en app.js

--- Tesis V2/p2p/src/components/Main.js
+++ Tesis V2/p2p/src/components/Main.js
@@ -33,54 +33,73 @@ class Main extends Component {
   render() {
     return (
       <div id="content">
-        <h1>Add Product</h1>
+        <h1>Añadir Producto</h1>
         <form onSubmit={(event) => {
           event.preventDefault();
           const name = this.productName.value;
-          const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'Ether'); // Asegúrate de que web3 está disponible
+          const price = window.web3.utils.toWei(this.productPrice.value.toString(), 'ether');
           this.props.createProduct(name, price);
         }}>
           <div className="form-group mr-sm-2">
             <input
               id="productName"
               type="text"
-              ref={(input) => { this.productName = input }}
+              ref={(input) => { this.productName = input; }}
               className="form-control"
-              placeholder="Product Name"
+              placeholder="Nombre del producto"
               required />
           </div>
           <div className="form-group mr-sm-2">
             <input
               id="productPrice"
               type="text"
-              ref={(input) => { this.productPrice = input }}
+              ref={(input) => { this.productPrice = input; }}
               className="form-control"
-              placeholder="Product Price"
+              placeholder="Precio en Ether"
               required />
           </div>
-          <button type="submit" className="btn btn-primary">Add Product</button>
+          <button type="submit" className="btn btn-primary">Añadir Producto</button>
         </form>
         <p>&nbsp;</p>
-        <h2>Buy Product</h2>
+        <h2>Comprar Productos</h2>
         <table className="table">
           <thead>
             <tr>
-              <th scope="col">#</th>
-              <th scope="col">Name</th>
-              <th scope="col">Price</th>
-              <th scope="col">Owner</th>
-              <th scope="col"></th>
+              <th>ID</th>
+              <th>Nombre</th>
+              <th>Precio</th>
+              <th>Propietario</th>
+              <th>Comprado</th>
+              <th>Acción</th>
             </tr>
           </thead>
-          <tbody id="productList">
-            <tr>
-              <th scope="row">1</th>
-              <td>iPhone X</td>
-              <td>1 Eth</td>
-              <td>0x39C7BC5496f4eaaa1fF75d88E079C22f0519E7b9</td>
-              <td><button className="buyButton">Buy</button></td>
-            </tr>
-            {/* Más productos... */}
+          <tbody>
+            {this.props.products.map((product, key) => {
+              return (
+                <tr key={key}>
+                  <td>{product.id.toString()}</td>
+                  <td>{product.name}</td>
+                  <td>{window.web3.utils.fromWei(product.price.toString(), 'Ether')} Eth</td>
+                  <td>{product.owner}</td>
+                  <td>{product.purchased ? "Sí" : "No"}</td>
+                  <td>
+                    {!product.purchased
+                      ? <button
+                          className="btn btn-success"
+                          name={product.id}
+                          value={product.price}
+                          onClick={(event) => {
+                            this.props.purchaseProduct(event.target.name, event.target.value);
+                          }}
+                        >
+                          Comprar
+                        </button>
+                      : null
+                    }
+                  </td>
+                </tr>
+              );
+            })}
           </tbody>
         </table>
       </div>

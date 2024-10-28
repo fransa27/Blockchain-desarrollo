22bda555a4cdc401ae0fa1cd382d4bc557b41710
fvsoto <102995723+fransa27@users.noreply.github.com>
2024-10-28 18:02:05 -0300

    agregar valor de voltaje en PV

--- Tesis V2/p2p/src/components/PVSystem.js
+++ Tesis V2/p2p/src/components/PVSystem.js
@@ -2,6 +2,8 @@ import React, { useEffect, useState } from 'react';
 import { Line } from 'react-chartjs-2';
 import { Chart as ChartJS, CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend } from 'chart.js';
 import jsonData from '../data/data.json'; // Importa tu archivo JSON
+import axios from 'axios';
+
 
 ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend);
 
@@ -30,10 +32,49 @@ const PVSystem = () => {
     });
   }, []);
 
+  //node-red
+  const [PVVoltage, setPVVoltaje] = useState(null);  // Voltaje 
+
+  useEffect(() => {
+    const fetchPV = async () => {
+      try {
+        const response = await axios.get('http://127.0.0.1:1880/api/pv');
+        setPVVoltaje(response.data);
+        console.log("response node-red pv: ", response);
+        console.log("data response node-red pv: ", response.data);
+      } catch (error) {
+        console.error('Error al obtener el voltaje:', error);
+      }
+    };
+    // Actualiza el voltaje cada 20 segundos
+    const interval = setInterval(fetchPV, 20000);
+
+    // Limpia el intervalo al desmontar el componente
+    return () => clearInterval(interval);
+  }, []);
+
   return (
     <div>
       <h2>Gráfico de ejemplo</h2>
       <Line data={chartData} />
+
+      <h1>Monitor de Voltaje en las Baterias</h1>
+      <div style={{ display: 'flex', alignItems: 'center', gap: '10px' }}>
+      <h2>Voltaje actual:</h2>
+      <p
+        style={{
+          color: 'white',
+          backgroundColor: 'green',
+          padding: '10px',
+          borderRadius: '5px',
+          display: 'inline-block'
+        }}
+      > 
+        {PVVoltage !== null ? `${PVVoltage} V` : 'Cargando...'}
+      </p> 
+      </div>
+
+
     </div>
   );
 };
